0698a414244bf285148a60c77891f05af2603c5b
Sepid Hosseini <56614286+sepidsh@users.noreply.github.com>
2021-11-18 12:27:29 -0800

    Add files via upload

---
+++ raster_to_json.py
@@ -0,0 +1,299 @@
+
+import os
+import json
+import argparse
+import json
+import open3d
+import pymesh
+import numpy as np
+import matplotlib.pyplot as plt
+from shapely.geometry import Polygon
+from descartes.patch import PolygonPatch
+from read_dd import read_data
+from misc.figures import plot_coords
+from misc.colors import colormap_255, semantics_cmap
+
+
+def raster_to_json(line):
+    """ convert extracted data from rasters to housegan ++ data format :  extract rooms type, bbox, doors, edges and neigbour rooms
+                
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    bbox_x1=[]
+    bbox_y1=[]
+    bbox_x2=[]
+    bbox_y2=[]
+    walls=[]
+
+    room_type,poly,doors_, walls,out=read_data(line)
+    d=[]
+    all_doors=[]
+    for i in range(1,len(doors_)+1):
+        if((i)%4==0 ) & (i+1!=1):
+            d.append(doors_[i-1])
+            all_doors.append(d)
+            d=[]
+        elif(i==1):
+            d=[]
+        if(i%4!=0):
+            d.append(doors_[i-1])
+    kh=0
+    al_dr=0
+    for hd in range(len(all_doors)):
+        dr_t=[]
+        dr_in=[]
+        doors=all_doors[hd]
+        d_t=2
+        t_x=abs(doors[0][1]-doors[1][1])
+        t_y=abs(doors[0][0]-doors[3][0])
+        ss=t_x
+        if(t_x>t_y):
+           d_t=1
+           ss=t_y
+        elif(t_x<t_y):
+           d_t=3
+        for pmc in range(5):
+            for dw in range(len(doors)):      
+                for nw in range(len(walls)) :
+                    if(walls[nw][5]==17):
+                        continue
+                    if(walls[nw][5]==15):
+                        continue
+                    if (d_t<=2) &(doors[dw][0]-doors[dw][2]<=1)& (walls[nw][0]-walls[nw][2]<=1)& (abs(doors[dw][0]-walls[nw][0])<=(ss-1)) & (abs(doors[dw][2]-walls[nw][2])<=(ss-1)): 
+                        l=doors[dw][1]
+                        r=doors[dw][3]
+                        if(l>r):
+                            t=l
+                            l=r
+                            r=t
+                        l_=walls[nw][1]
+                        r_=walls[nw][3]
+                        if(l_>r_):
+                            t=l_
+                            l_=r_
+                            r_=t
+                        if (((r-r_)<=pmc )& (pmc>=(l_-l))) :
+                            if(len(dr_in)<2):
+                                if(walls[nw][6] not in dr_t):
+                                    dr_t.append(walls[nw][6])
+                                    dr_in.append(nw)
+                               
+                    elif (d_t>=2)& (doors[dw][1]-doors[dw][3]<=1)& (walls[nw][1]-walls[nw][3]<=1) &(abs(doors[dw][1]-walls[nw][1])<=(ss-1)) & (abs(doors[dw][3]-walls[nw][3])<=(ss-1)): 
+                        l=doors[dw][0]
+                        r=doors[dw][2]
+                        if(l>r):
+                            t=l
+                            l=r
+                            r=t
+                        l_=walls[nw][0]
+                        r_=walls[nw][2]
+                        if(l_>r_):
+                            t=l_
+                            l_=r_
+                            r_=t
+                        if(((r-r_)<=pmc )& (pmc>=(l_-l))):
+                            if(len(dr_in)<2):
+                                if(walls[nw][6] not in dr_t):
+                                    dr_t.append(walls[nw][6])
+                                    dr_in.append(nw)
+        if(len(dr_t)==2):
+            walls[dr_in[0]][8]=walls[dr_in[1]][5]
+            walls[dr_in[0]][7]=walls[dr_in[1]][6]    
+            walls[dr_in[1]][8]=walls[dr_in[0]][5]
+            walls[dr_in[1]][7]=walls[dr_in[0]][6]  
+            al_dr=al_dr+1
+                
+        else:
+            print("sometime not 2 dooor",hd,doors)		
+    
+        assert(len(dr_t)<=2)
+  
+
+    assert(al_dr==(len(all_doors)-1))
+    
+    
+    omn=[]
+    tr=0
+    en_pp=0
+    for nw in range(len(walls)-(len(all_doors)*4),len(walls)):
+        if(tr%4==0):
+            omn=[]
+        tr=tr+1
+        for kw in range(len(walls)-(len(all_doors)*4)+1):
+            if(walls[kw][5]==17)&(walls[nw][5]==17):
+                continue		
+            if(walls[kw][5]==15)&(walls[nw][5]==15):
+                continue		
+            if(walls[kw][5]==15)&(walls[nw][5]==17):
+                continue		
+            for pmc in range (5):
+                if(abs(walls[kw][0]-walls[nw][0])<=(ss-1)) & (abs(walls[kw][2]-walls[nw][2])<=(ss-1)):
+                    l=walls[kw][1]
+                    r=walls[kw][3]
+                    if(l>r):
+                        t=l
+                        l=r
+                        r=t
+                    l_=walls[nw][1]
+                    r_=walls[nw][3]
+                    if(l_>r_):
+                        t=l_
+                        l_=r_
+                        r_=t
+                    if(pmc>=r_-r )& (l-l_<=pmc) &( nw!=kw):
+                        if(walls[nw][5]==17) &(walls[nw][8]==0)& (walls[kw][6] not in omn):
+                            walls[nw][8]=walls[kw][5]
+                            walls[nw][7]=walls[kw][6]    
+                            omn.append(walls[kw][6])
+                        if(walls[nw][5]==15) &(walls[nw][8]==0):
+                            walls[nw][8]=walls[kw][5]
+                            walls[nw][7]=walls[kw][6] 
+                            en_pp=1 
+                 
+                if(abs(walls[kw][1]-walls[nw][1])<=(ss-1)) & (abs(walls[kw][3]-walls[nw][3])<=(ss-1)):
+                    l=walls[kw][0]
+                    r=walls[kw][2]
+                    if(l>r):
+                        t=l
+                        l=r
+                        r=t
+                    l_=walls[nw][0]
+                    r_=walls[nw][2]
+                    if(l_>r_):
+                        t=l_
+                        l_=r_
+                        r_=t
+                    if(pmc>=r_-r )& (l-l_<=pmc) &( nw!=kw):
+                        if(walls[nw][5]==17) & (walls[nw][8]==0)& (walls[kw][6] not in omn):
+                            walls[nw][8]=walls[kw][5]
+                            walls[nw][7]=walls[kw][6]    
+                            omn.append(walls[kw][6])
+                
+                        if(walls[nw][5]==15) & (walls[nw][8]==0):
+                            walls[nw][8]=walls[kw][5]
+                            walls[nw][7]=walls[kw][6]    
+                            en_pp=1
+         
+                    
+    for i in range(1):
+        for iw in range(len(walls)):
+            tp_out=-1
+            dif_x=10
+            dif_y=10
+
+            type_out=0
+        for jw in range(len(walls)):
+            if(walls[iw][0]==walls[iw][2]):
+                if (walls[jw][0]!=walls[jw][2]):
+                    continue
+                if ((walls[iw][0]-walls[jw][0])==(walls[iw][2]- walls[jw][2])):
+                    rnp=walls[jw][1]
+                    fnp=walls[jw][3]
+                    rmp=walls[iw][1]
+                    fmp=walls[iw][3]
+                    if( rnp<fnp):
+                        t=fnp
+                        fnp=rnp
+                        rnp=t
+                    if(rmp<fmp):
+                        t=fmp
+                        fmp=rmp
+                        rmp=t
+                    if(abs(rmp)<=abs(rnp))| (abs(fmp)<=abs(fnp)):
+                        dif_x_temp=walls[iw][0]-walls[jw][0]
+                        if(abs(dif_x)>abs(dif_x_temp)) & (iw!=jw):
+                            dif_x=dif_x_temp
+                            tp_out=walls[jw][6]
+                            type_out=walls[jw][5]
+                          
+            elif(walls[iw][1]==walls[iw][3]):
+                if ((walls[iw][1]-walls[jw][1])==(walls[iw][3]- walls[jw][3])) :           
+                    rnp=walls[jw][0]
+                    fnp=walls[jw][2]
+                    rmp=walls[iw][0]
+                    fmp=walls[iw][2]
+                    if( rnp<fnp):
+                        t=fnp
+                        fnp=rnp
+                        rnp=t
+                    if(rmp<fmp):
+                        t=fmp
+                        fmp=rmp
+                        rmp=t
+                    if(abs(rmp)<=abs(rnp))| (abs(fmp)<=abs(fnp)):
+                        dif_y_temp=walls[iw][1]-walls[jw][1]
+                        if(abs(dif_y)>abs(dif_y_temp))&( iw!=jw ):
+                            dif_y=dif_y_temp
+                            tp_out=walls[jw][6]
+                            type_out=walls[jw][5]
+      
+    km=0
+    assert(en_pp==1) #throwing out really strange layouts
+    
+    
+    lenx=1.0
+    leny=1.0
+    min_x=0.0
+    min_y=0.0
+    bboxes=[]  
+    edges=[]
+    ed_rm=[]
+    info=dict()
+
+    #  The edges for the graph
+    for w_i in range(len(walls)):
+        edges.append([((walls[w_i][0]-min_x)/lenx),((walls[w_i][1]-min_y)/leny),((walls[w_i][2]-min_x)/lenx),((walls[w_i][3]-min_y)/leny),walls[w_i][5],walls[w_i][8]])
+        if(walls[w_i][6]==-1):
+            ed_rm.append([walls[w_i][7]])
+        elif(walls[w_i][7]==-1): 
+            ed_rm.append([walls[w_i][6]])
+        else:
+            ed_rm.append([walls[w_i][6],walls[w_i][7]])
+    
+    #  The bbox for room masks
+    for i in range(len(poly)):
+        p=poly[i]
+        pm=[]
+        for p_i in range((p)):
+            pm.append(([edges[km+p_i][0],edges[km+p_i][1]]))
+        km=km+p
+        polygon = Polygon(pm)
+        plot_coords(ax, polygon.exterior, alpha=0)
+        bbox=np.asarray(polygon.bounds)
+        bboxes.append(bbox.tolist())
+     
+        
+        patch = PolygonPatch(polygon, facecolor=semantics_cmap["bedroom"], alpha=0.7)
+        ax.add_patch(patch)
+   
+    info['room_type'] = room_type
+    info['boxes'] = bboxes
+    info['edges'] = edges
+    info['ed_rm'] = ed_rm
+
+    print(bboxes)
+   
+  
+    ### saving json files
+    with open("../../../../../guriv-64/hg_rooms/"+line[17:-4]+".json","w") as f:
+         json.dump(info, f)
+
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Structured3D 3D Visualization")
+    parser.add_argument("--path", required=True,
+                        help="dataset path", metavar="DIR")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    line=args.path 
+    raster_to_json(line)
+
+
+if __name__ == "__main__":
+    main()
---
+++ read_dd.py
@@ -0,0 +1,357 @@
+import cv2 
+import numpy as np 
+from PIL import Image
+import matplotlib.pyplot as plt
+import mask_to_poly as mr
+
+def read_door(door_img,img,tmp_diff):
+	tmp3=door_img.copy()
+	tmp4=door_img.copy()
+	for k in range(256):
+		for h in range(256):
+			has=[]
+			for knm in range(10):
+				has.append(0)
+			if(tmp4[k][h]==1):
+				p=img[k-tmp_diff-4:k+tmp_diff+4,h-tmp_diff-4:h+tmp_diff+4,2]
+				s=p[np.nonzero(p)]
+				if(len(s)==0):
+					continue
+				r=[]
+				kmmmm=s[0]
+				for t in range(len(s)):
+					if(t==0):
+						r.append(s[t])
+					elif(s[t] not in r):
+						r.append(s[t])
+				if(len(r)>=3):
+					 tmp3[k][h]=0	
+				elif(len(r)==2):
+					has[r[0]]=1
+					has[r[1]]=1
+					tmp3[k][h]= has[0]*1+has[1]*2+has[3]*4+has[4]*8+has[5]*16+has[6]*32+has[7]*64+has[8]*128+has[9]*256
+	s=np.unique(tmp3)
+	tmp4=tmp3.copy()
+	for ks in range(len(s)):
+		for k in range(256):
+			for h in range(256):
+				if(tmp3[k][h]==s[ks]):
+					tmp4[k][h]=int(ks)
+	return tmp4
+
+
+
+def sort_corners(corners ,k_d):
+	coords=[]
+	ind=[]
+	if(k_d==0):
+		for j in range(len(corners)):
+			ind.append(0)
+		for i in range(len(corners)):
+			if (i==0):
+				coords.append(corners[0])
+				ind[i]=1
+
+			elif(i%2==1):
+				k=coords[i-1][0]
+				for j in range(len(corners)):
+					if(corners[j][0]==k)& ( ind[j]!=1):
+						coords.append(corners[j])
+						ind[j]=1
+						break
+			elif(i%2==0):
+				k=coords[i-1][1]
+				for j in range(len(corners)):
+					if(corners[j][1]==k)&  (ind[j]!=1):
+						coords.append(corners[j])
+						ind[j]=1
+						break
+					
+	if(k_d==1):
+		for s in range(len(corners)):
+			ind.append(0)
+		p=0		
+		for i in range(len(corners)):
+			if (i%4==0):
+				coords.append(corners[i])
+				ind[i]=1
+				p=p+1
+			elif(i%2==1):
+				k=coords[i-1][0]
+				tk=coords[i-1][1]  
+				pp=0
+				for j in range(len(corners)):
+					if(corners[j][0]==k)& (ind[j]!=1):
+						if(pp==0) :
+							pp=pp+1
+							coords.append(corners[j])
+							fn=j
+						else:
+							if (abs(corners[j][1]-tk)<=abs(coords[i][1]-tk)) &(abs(corners[j][1]-tk)!=0):
+								coords[i]=corners[j]
+								fn=j
+				ind[fn]=ind[fn]+1
+				fn=-1		
+				p=p+1
+			elif(i%2==0)& (i%4!=0):
+				p=p+1
+				k=coords[i-1][1]
+				tk=coords[i-1][0] 
+				pp=0
+				for j in range(len(corners)):
+					if(corners[j][1]==k) & (ind[j]!=1):
+						if(pp==0):
+							pp=pp+1
+							coords.append(corners[j])
+							fn=j  						
+						else:
+							if (abs(corners[j][0]-tk)<=abs(coords[i][0]-tk)) &(abs(corners[j][0]-tk)!=0):
+								coords[i]=corners[j]
+								fn=j  
+				ind[fn]=ind[fn]+1 
+				fn=-1
+	return coords
+			
+def read_data(line):
+	poly=[]
+	img = np.asarray(Image.open(line))
+	dec=0
+	img_room_type=img[:,:,1]
+	img_room_number=img[:,:,2]
+	wall_img=np.zeros((256, 256))
+	for k in range(256):
+		for h in range(256):
+			if(img_room_type[k][h]==16):
+				wall_img[k][h]=16		
+			if(img_room_type[k][h]==17):
+				wall_img[k][h]=10
+	room_no=img_room_number.max()
+	room_imgs=[]
+	rm_types=[]
+	for i in range(room_no):
+		room_img=np.zeros((256, 256))
+		for k in range(256):
+			for h in range(256):
+				if(img_room_number[k][h]==i+1):
+					room_img[k][h]=1
+					k_=k
+					h_=h
+		rm_t=img_room_type[k_][h_]
+
+		#changing rplan rooms_type to housegan++ rooms_type
+		if(rm_t==0):
+			rm_types.append(1)		
+		elif(rm_t==1):
+			rm_types.append(3)
+		elif(rm_t==2):
+			rm_types.append(2)		
+		elif(rm_t==3):
+			rm_types.append(4)
+		elif(rm_t==4):
+			rm_types.append(7)
+		elif(rm_t==5):
+			rm_types.append(3)
+		elif(rm_t==6):
+			rm_types.append(8)
+		elif(rm_t==7):
+			rm_types.append(3)
+		elif(rm_t==8):
+			rm_types.append(3)
+		elif(rm_t==9):
+			rm_types.append(5)
+		elif(rm_t==10):
+			rm_types.append(6)
+		elif(rm_t==11):
+			rm_types.append(10)
+		else:
+			rm_types.append(16)
+		room_imgs.append(room_img)
+
+	walls=[]
+	doors=[]
+	rm_type=rm_types
+	for t in range(len(room_imgs)):
+		tmp=room_imgs[t]
+		for k in range(254):
+			for h in range(254):
+				if(tmp[k][h]==1) & (tmp[k+1][h]==0) & (tmp[k+2][h]==1):
+					tmp[k+1][h] =1				
+		for k in range(254):
+			for h in range(254):
+				if(tmp[h][k]==1) & (tmp[h][k+1]==0) & (tmp[h][k+2]==1):
+					tmp[h][k+1] =1				
+		for k in range(254):
+			for h in range(254):
+				if(tmp[k][h]==0) & (tmp[k+1][h]==1) & (tmp[k+2][h]==0):
+					tmp[k+1][h] =0				
+		for k in range(254):
+			for h in range(254):
+				if(tmp[h][k]==0) & (tmp[h][k+1]==1) & (tmp[h][k+2]==0):
+					tmp[h][k+1] =0
+
+		room_imgs[t]=tmp
+		poly2=mr.get_polygon(room_imgs[t])
+		coords_1=list(poly2.exterior.coords)
+		coords=[]
+		for kn in range(len(coords_1)):
+			coords.append([list(coords_1[kn])[1],list(coords_1[kn])[0],0,0,t,rm_type[t]]) 
+		p=0
+		for c in range(len(coords)-1):
+			walls.append([coords[c][0],coords[c][1],coords[c+1][0],coords[c+1][1],-1,coords[c][5],coords[c][4],-1,0])
+		p=len(coords)-1
+		poly.append(p)		
+	tmp=img[:,:,1]
+
+
+	door_img=np.zeros((256, 256))
+	doors_img=[]		
+	for k in range(256):
+		for h in range(256):
+			if(tmp[k][h]==17):
+				door_img[k][h]=1
+	
+	tmp=door_img
+	rms_type=rm_type
+	coords=[]
+	for k in range(2,254):
+		for h in range(2,254):
+			if(tmp[k][h]==1):
+				if((tmp[k-1][h]==0) & (tmp[k-1][h-1]==0)&(tmp[k][h-1]==0)):
+					coords.append([h,k,0,0])
+				elif (tmp[k+1][h]==0)&(tmp[k+1][h-1]==0)&(tmp[k][h-1]==0):
+					coords.append([h,k,0,0])
+				elif (tmp[k+1][h]==0)&(tmp[k+1][h+1]==0)&(tmp[k][h+1]==0): 
+					coords.append([h,k,0,0])
+				elif (tmp[k-1][h]==0)&(tmp[k-1][h+1]==0)&(tmp[k][h+1]==0): 
+					coords.append([h,k,0,0])					
+				elif(tmp[k+1][h]==1)&(tmp[k][h+1]==1)& (tmp[k+1][h+1]==0):
+					coords.append([h,k,0,0])					
+				elif(tmp[k-1][h]==1)&(tmp[k][h+1]==1)& (tmp[k-1][h+1]==0):
+					coords.append([h,k,0,0])					
+				elif(tmp[k+1][h]==1)&(tmp[k][h-1]==1)&(tmp[k+1][h-1]==0) : 
+					coords.append([h,k,0,0])				
+				elif(tmp[k-1][h]==1) & (tmp[k][h-1]==1) & (tmp[k-1][h-1]==0):
+					coords.append([h,k,0,0])
+					
+	tmp_diff=1000000   
+	p_x_1=coords[0][0]
+	for k in range(1, len(coords)):
+		p_x_2=coords[k][0]
+		tmp_dif=abs(p_x_1-p_x_2)
+		if(tmp_dif<tmp_diff)&(tmp_dif>1):
+			tmp_diff=tmp_dif
+	p_y_1=coords[0][1]
+	for k in range(1, len(coords)):
+		p_y_2=coords[k][1]
+		tmp_dif=abs(p_y_1-p_y_2)
+		if(tmp_dif<tmp_diff)&(tmp_dif>1):
+			tmp_diff=tmp_dif
+	
+	door_imgs=read_door(door_img,img,tmp_diff)
+	door_no=int(door_imgs.max())
+	door_tp=[]
+	for i in range(door_no):
+		door_img=np.zeros((256, 256))
+		for k in range(256):
+			for h in range(256):
+				if(door_imgs[k][h]==i+1):door_img[k][h]=1
+		doors_img.append(door_img)
+	rmpn=len(doors_img)				
+	for t in range(len(doors_img)):
+		tmp=doors_img[t]
+		kpp=np.max(tmp)
+		if(kpp<=0):
+			dec=dec+1
+			continue
+		poly2=mr.get_polygon(doors_img[t])
+		coords_1=list(poly2.exterior.coords)#
+		coords=[]
+		for kn in range(len(coords_1)):
+			coords.append([list(coords_1[kn])[1],list(coords_1[kn])[0],0,0,t,17]) 
+		p=0
+		for c in range(len(coords)-1):
+			walls.append([coords[c][0],coords[c][1],coords[c+1][0],coords[c+1][1],-1,17,len(rms_type)+coords[c][4]-dec,-1,0])
+			doors.append([coords[c][0],coords[c][1],coords[c+1][0],coords[c+1][1]])
+		p=len(coords)-1
+		poly.append(p)
+	tmp=img[:,:,1]
+	en_img=np.zeros((256, 256))
+	for k in range(256):
+		for h in range(256):
+			if(tmp[k][h]==15):
+				en_img[k][h]=1
+	tmp=en_img
+	coords=[]
+	for k in range(2,254):
+		for h in range(2,254):
+			if(tmp[k][h]==1):
+				if((tmp[k-1][h]==0) & (tmp[k-1][h-1]==0)&(tmp[k][h-1]==0)):
+					coords.append([h,k,0,0])
+				elif (tmp[k+1][h]==0)&(tmp[k+1][h-1]==0)&(tmp[k][h-1]==0):
+					coords.append([h,k,0,0])
+				elif (tmp[k+1][h]==0)&(tmp[k+1][h+1]==0)&(tmp[k][h+1]==0): 
+					coords.append([h,k,0,0])
+				elif (tmp[k-1][h]==0)&(tmp[k-1][h+1]==0)&(tmp[k][h+1]==0): 
+					coords.append([h,k,0,0])
+				elif(tmp[k+1][h]==1)&(tmp[k][h+1]==1)& (tmp[k+1][h+1]==0):
+					coords.append([h,k,0,0])					
+				elif(tmp[k-1][h]==1)&(tmp[k][h+1]==1)& (tmp[k-1][h+1]==0):
+					coords.append([h,k,0,0])					
+				elif(tmp[k+1][h]==1)&(tmp[k][h-1]==1)&(tmp[k+1][h-1]==0) : 
+					coords.append([h,k,0,0])					
+				elif(tmp[k-1][h]==1) & (tmp[k][h-1]==1) & (tmp[k-1][h-1]==0):
+					coords.append([h,k,0,0])
+					
+	en_imgs=[]
+	for i in range(1):
+
+		door_img=np.zeros((256, 256))
+		for k in range(256):
+			for h in range(256):
+				if(en_img[k][h]==i+1):
+					en_img[k][h]=1
+		en_imgs.append(en_img)
+	for t in range(len(en_imgs)):
+		tmp=en_imgs[t]
+		kpp=np.max(tmp)
+		if(kpp<=0):
+			dec=dec+1
+			continue
+		poly2=mr.get_polygon(en_imgs[t])
+		coords_1=list(poly2.exterior.coords)
+		coords=[]
+		for kn in range(len(coords_1)):
+			coords.append([list(coords_1[kn])[1],list(coords_1[kn])[0],0,0,t,15]) 
+		p=0
+		for c in range(len(coords)-1):
+			walls.append([coords[c][0],coords[c][1],coords[c+1][0],coords[c+1][1],-1,15,rmpn+len(rms_type)+coords[c][4]-dec,-1,0])
+			doors.append([coords[c][0],coords[c][1],coords[c+1][0],coords[c+1][1]])
+		p=len(coords)-1
+		poly.append(p)
+	
+	no_doors=int(len(doors)/4)
+	rms_type=rm_type
+	for i in range(no_doors-1):
+		rms_type.append(17)
+	rms_type.append(15)
+	out=1
+	### check if it was noy polygon 
+	for i in range(len(poly)):
+		if(poly[i]<4):
+			out=-1
+	if (len(doors)%4!=0):
+			out=-3	
+	##saving the name out standard (usable) layout		
+	if(out!=1):
+		h.write(line)
+	"""f=open("output.txt", "a+")
+	f.write(str(rms_type).strip('[]'))
+	f.write("   ")
+	f.write(str(len(rms_type)))
+	if((len(rms_type)-no_doors)>(no_doors)):
+		h1=open("door.txt","a+")
+		out=-4	
+		h1.write(line)"""
+	assert(out==1)
+	return rms_type,poly,doors,walls,out
+	
